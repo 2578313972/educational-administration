2118ebb0edaee6872066eb58d85ea84e34cda59e
2578313972 <2578313972@q2578313972@qq.com>
2020-04-15 01:55:15 +0800

    fix Router

--- src/views/home.vue
+++ src/views/home.vue
@@ -2,13 +2,13 @@
     <div id="">
         <el-container>
             <el-aside width="200px">
-                <div style="height:70px;"></div>
+                <div style="height:70px;background:#545c64;"></div>
                 <el-menu
                 :style="{height:height-70+'px'}"
                 background-color="#545c64"
                 text-color="#fff"
                 active-text-color="#ffd04b"
-                default-active="1-4-1"
+                :default-active="active"
                 class="el-menu-vertical-demo"
                 :collapse="isCollapse">
                     <el-submenu :index="index.toString()" v-for="(item, index) in msg" :key="item.name">
@@ -17,19 +17,18 @@
                         <span slot="title">{{item.name}}</span>
                         </template>
                         <el-menu-item-group>
-                            <el-menu-item v-for="(data, index2) in item.data" :key="index2" @click="seleaddTab(item,index2)" :index="index.toString()+index2.toString()">{{data.name}}</el-menu-item>
+                            <el-menu-item v-for="(data, index2) in item.data" :key="index2" @click="seleaddTab(item,index2)" :index="data.url">{{data.name}}</el-menu-item>
                         </el-menu-item-group>
                     </el-submenu>
                 </el-menu>
             </el-aside>
             <el-main>
-                <el-tabs v-model="editableTabsValue" type="card" closable @tab-remove="removeTab">
+                <el-tabs v-model="editableTabsValue" type="card" closable @tab-remove="removeTab" @tab-click="tabclick">
                 <el-tab-pane
                     v-for="(item) in editableTabs"
                     :key="item.name"
                     :label="item.title"
                     :name="item.name"
-                    @tab-click="tabclick"
                 >
                     <router-view></router-view>
                 </el-tab-pane>
@@ -45,6 +44,8 @@
             return {
                 height:0,
                 isCollapse:false,
+                addNum:'0',
+                active:"FrontOne",
                 msg:[
                     {
                         name:"Student",
@@ -61,55 +62,61 @@
                     }
                 ],
                 editableTabsValue: '1',// 默认显示
-                editableTabs: [],
-                tabIndex: 2
+                historeTabs:[],
+                editableTabs: [
+                    {title:"获取班级所有信息",name:"1",url:"FrontOne"}
+                ]
             }
         },
         created() {
             this.height = window.innerHeight
+            this.addNum = this.editableTabs.length
         },
         methods: {
-            seleaddTab(e,index){
-                console.log(e,index)
-
+            seleaddTab(e,index){ // 添加选项卡
+                this.active = e.data[index].url
                 for(let i in this.editableTabs){
                     if(this.editableTabs[i].title === e.data[index].name){
+                        if(this.$route.name !== this.editableTabs[i].url) this.$router.push(this.editableTabs[i].url)
+                        this.editableTabsValue = this.editableTabs[i].name
                         return
                     }
                 }
-
-
                 this.$router.push(e.data[index].url)
-                this.editableTabs.push({title: e.data[index].name,name: (this.editableTabs.length+1).toString() ,url:e.data[index].url})
+                this.editableTabs.push({title: e.data[index].name,name: (this.addNum+1).toString() ,url:e.data[index].url})
+                this.addNum++
+                this.editableTabsValue = this.addNum.toString()
             },
-            tabclick(e){
-                console.log(e)
+            tabclick(e){ // 点击选项卡
+                this.active = this.editableTabs[e.index].url
+                if(this.$route.name !== this.editableTabs[e.index].url) this.$router.push(this.editableTabs[e.index].url)
             },
-            addTab(targetName) {
-                let newTabName = ++this.tabIndex + '';
-                this.editableTabs.push({
-                title: 'New Tab',
-                name: newTabName,
-                content: 'New Tab content'
-                });
-                this.editableTabsValue = newTabName;
-            },
-
-            removeTab(targetName) {
+            removeTab(targetName) { // 删除选项卡
                 let tabs = this.editableTabs;
                 let activeName = this.editableTabsValue;
-                if (activeName === targetName) {
+                if (activeName == targetName) {
                     tabs.forEach((tab, index) => {
-                        if (tab.name === targetName) {
+                        if (tab.name == targetName) {
+                            if(this.historeTabs[0]){
+
+                            }
                             let nextTab = tabs[index + 1] || tabs[index - 1];
                             if (nextTab) {
+                                this.active = nextTab.url
                                 activeName = nextTab.name;
                             }
+                            this.active = ""
                         }
                     });
                 }
                 this.editableTabsValue = activeName;
                 this.editableTabs = tabs.filter(tab => tab.name !== targetName);
+            }
+        },
+        watch:{
+            active(){
+                this.historeTabs.unshift(this.active)
+                this.historeTabs = [...new Set(this.historeTabs)]
             }
         }
     };
